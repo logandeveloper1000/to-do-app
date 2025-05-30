c854061357b29bcc90fd8a680355ac2048065057
connectionaccounting-systems <gabriel.connectionacc@gmail.com>
2025-05-29 21:55:41 -0400

    updating

--- src/firebase.js
+++ src/firebase.js
@@ -7,15 +7,27 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD54kGOh-xLMeAh_RX5bu8TRMZliNhOB4A",
-  authDomain: "portfolio-logandeveloper.firebaseapp.com",
-  projectId: "portfolio-logandeveloper",
-  storageBucket: "portfolio-logandeveloper.firebasestorage.app",
-  messagingSenderId: "546326089695",
-  appId: "1:546326089695:web:06d1bee3fff4fbb396fe6a",
-  measurementId: "G-CSKD10JM48"
+  apiKey: "AIzaSyB1TWj5WC5yEWYmQCkc8pqx5xvrkhEbN3A",
+  authDomain: "logan-portolio.firebaseapp.com",
+  projectId: "logan-portolio",
+  storageBucket: "logan-portolio.firebasestorage.app",
+  messagingSenderId: "254880871321",
+  appId: "1:254880871321:web:e72667874888aa824b5908",
+  measurementId: "G-KCPDNJ2LNG"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
